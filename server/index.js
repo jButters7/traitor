8d903bd32952eb6b15b1a4637dd8561ad8872ebd
Jake Butters <jacobbutters@gmail.com>
2020-12-01 20:23:30 -0700

    server

--- server/index.js
+++ server/index.js
@@ -6,14 +6,13 @@ const app = express();
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
 
 app.use(express.json());
-app.use(
-  session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }
-  })
-);
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 } //One year long cookie
+}))
 
 //TODO ENDPOINT GO HERE
 
@@ -25,7 +24,6 @@ massive({
 }).then(dbInstance => {
   app.set('db', dbInstance)
   console.log('DB is ready!')
-  app.listen(SERVER_PORT, () => {
-    console.log(`Sever is ready on port ${SERVER_PORT}`)
-  })
+  app.listen(SERVER_PORT, () => console.log(`Sever is ready on port ${SERVER_PORT}`)
+  )
 })
